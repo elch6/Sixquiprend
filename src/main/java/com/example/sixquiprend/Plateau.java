2c18ee0e02485cd43b0d1bc7afe3cbea84cd19e0
Elena <elena.charpentier@eleve.isep.fr>
2023-05-15 15:43:36 +0200

    creation joueur

--- src/main/java/com/example/sixquiprend/Plateau.java
+++ src/main/java/com/example/sixquiprend/Plateau.java
@@ -1,19 +1,70 @@
 package com.example.sixquiprend;
 
-import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Plateau {
-    @FXML
-    private Button carte1;
+public class Plateau extends Application {
+    @Override
+    public void start(Stage plateau) throws Exception {
+        HBox hboxCartes = new HBox();
+        hboxCartes.setPadding(new Insets(10, 10, 10, 10));
+        hboxCartes.setStyle("-fx-background-color: #D3D3D3; -fx-background-radius: 20;");
 
-    @FXML
-    public void setCarte1() {
-        Image image = new Image("C:\\Users\\elena\\test\\Sixquiprend\\src\\main\\resources\\com\\example\\sixquiprend\\cartes\\carte1.png");
-        ImageView imageView = new ImageView(image);
-        carte1.setGraphic(imageView);
+        for (int i = 0; i < 10; i++) {
+            Button bouton = new Button();
+            bouton.setStyle("-fx-background-color: #D3D3D3");
+            hboxCartes.getChildren().add(bouton);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Button bouton = (Button) hboxCartes.getChildren().get(i);
+            Image image = new Image("C:\\Users\\elena\\test\\Sixquiprend\\src\\main\\resources\\com\\example\\sixquiprend\\cartes\\carte1.png");
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(70);
+            imageView.setFitHeight(100);
+            bouton.setGraphic(imageView);
+        }
+
+        Label labelJoueur = new Label("Les cartes de");
+        labelJoueur.setStyle("-fx-font-weight: bold; -fx-font-size: 16;");
+        labelJoueur.setPadding(new Insets(10, 10, 10, 10));
+
+        GridPane plateauGrid = new GridPane();
+        plateauGrid.setPadding(new Insets(10, 10, 10, 10));
+        plateauGrid.setHgap(10);
+        plateauGrid.setVgap(10);
+
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 6; col++) {
+                Image image = new Image("C:\\Users\\elena\\test\\Sixquiprend\\src\\main\\resources\\com\\example\\sixquiprend\\cartes\\carte1.png");
+                ImageView imageView = new ImageView(image);
+                imageView.setFitWidth(70);
+                imageView.setFitHeight(100);
+                plateauGrid.add(imageView, col, row);
+            }
+        }
+
+        VBox vboxPlateau = new VBox();
+        vboxPlateau.setPadding(new Insets(10, 10, 10, 10));
+        vboxPlateau.setSpacing(5);
+        vboxPlateau.getChildren().addAll(labelJoueur, hboxCartes, plateauGrid);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setLeft(vboxPlateau);
+
+        Scene scene = new Scene(borderPane, 1280, 645);
+        plateau.setTitle("Six qui prend");
+        plateau.setScene(scene);
+        plateau.show();
     }
-
 }
